9b95e6eb882e14029c45fc96bd4df030b51313b8
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-18 07:44:30 +0100

    chore: add bash utils script

---
+++ scripts/utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+#######################################
+# Check if a command exists.
+# Arguments:
+#   Command to check, a name or path.
+# Returns:
+#   1 if command does not exist.
+#######################################
+function check_cmd() {
+  if [[ -z "$(command -v "$1")" ]]; then
+    err "Command '$1' does not exist."
+    exit 1
+  fi
+}
